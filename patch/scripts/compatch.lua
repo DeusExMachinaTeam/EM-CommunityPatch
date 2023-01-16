a3a2cd8966218e79ecdd224a21c3d223733daa88
zatinu322 <pavl05@mail.ru>
2023-01-16 23:55:32 +0300

    Removed r1m1 strings.xml from remaster folder, added special function for showing narrator\'s text in r1m1 to compatch.lua

--- patch/scripts/compatch.lua
+++ patch/scripts/compatch.lua
@@ -1,1 +1,7 @@
 CP_VERSION = "1.12"
+
+-- Отображение первого текста рассказчика
+-- вынесено сюда, так как текст имеет разную скорость в патче и ремастере
+function ShowFirstNarratorText()
+    AddCinematicMessage( 77771, 0.1)
+end
--- remaster/data/scripts/compatch.lua
+++ remaster/data/scripts/compatch.lua
@@ -1,1 +1,7 @@
 CP_VERSION = "1.12 HD"
+
+-- Отображение первого текста рассказчика
+-- вынесено сюда, так как текст имеет разную скорость в патче и ремастере
+function ShowFirstNarratorText()
+    AddCinematicMessage( 77772, 0.1)
+end
